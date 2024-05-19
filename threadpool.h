fece7c6243f8339e8ab388a19686d1fd0501fe85
luyuni <niyulu695@163.com>
2024-05-19 13:53:37 +0800

    init

---
+++ threadpool.h
@@ -0,0 +1,33 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <pthread.h>
+#include <list>
+#include <locker.h>
+
+// 定义成模板类是为了代码的复用，参数T表示任务类
+template<typename T>
+class threadpool {
+private:
+    // 线程的数量
+    int m_thread_number;
+    // 线程池的数组
+    pthread_t * m_threads;
+    // 队列大小
+    int m_max_requests;
+    // 队列实例
+    std::list m_workqueue;
+    // 互斥锁
+    locker m_queuelocker;
+    // 信号量
+    sem m_queuestat;
+    // 是否结束线程
+    bool m_stop;
+public:
+    thread_pool(int thread_num = 8, int m_max_requests = 1000);
+    ~threadpool();
+    bool append(T * request);
+};
+
+
+#endif
