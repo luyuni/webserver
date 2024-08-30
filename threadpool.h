71857a20bc764139b719f9d741db19f6651ee6d1
luyuni <niyulu695@163.com>
2024-08-30 22:00:23 +0800

    fix

--- threadpool.h
+++ threadpool.h
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <list>
 #include <locker.h>
+#include <exception>
+#include <cstdio>
 
 // 定义成模板类是为了代码的复用，参数T表示任务类
 template<typename T>
@@ -31,6 +33,32 @@ public:
 
 
 template<typename T>
+threadpool<T>::thread_pool(int thread_number, int max_requests) :
+    m_thread_number(thread_number), m_max_request(max_requests),
+    m_stop(false), m_thread(NULL) {
+        if (thread_number <= 0 || max_requests <= 0) {
+            throw std::exception();
+        }
+        m_threads = new pthread_t[m_thread_number];
+
+        if (! m_threads) {
+            throw std::exception();
+        }
+
+        for (int i = 0; i < thread_number, i ++) {
+            printf("create the %dth thread", i);
+            if (pthread_create(m_threads + i, NULL, worker, NULL) != 0) {
+                delete [] m_threads;
+                throw std::exception();
+            }
+
+            if (pthread_detach(m_thread[i])) {
+                delete[] m_threads;
+                throw std::exception();
+            }
+        }
+
+    }
 
 
 #endif
